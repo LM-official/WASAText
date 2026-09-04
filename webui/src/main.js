66cfbfb143c33ee6eb4ea0d3f2e2f233e8308067
LM-official <lorymerc2004@gmail.com>
2026-09-04 16:00:44 +0200

    Boot frontend

--- webui/src/main.js
+++ webui/src/main.js
@@ -1,4 +1,4 @@
-import {createApp, reactive} from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from './services/axios.js';
@@ -7,6 +7,7 @@ import LoadingSpinner from './components/LoadingSpinner.vue'
 
 import './assets/dashboard.css'
 import './assets/main.css'
+import './assets/wasatext.css'
 
 const app = createApp(App)
 app.config.globalProperties.$axios = axios;
